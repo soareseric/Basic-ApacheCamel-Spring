606ea38e0c0962d475496a348e1f7fc2484571a4
soareseric <contato.eric.sm@gmail.com>
2018-12-13 11:32:14 -0200

    Projeto criado

---
+++ src/main/java/com/ericsoarescamel/ApacheCamelApplication.java
@@ -0,0 +1,14 @@
+package com.ericsoarescamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCamelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCamelApplication.class, args);
+	}
+
+}
+
